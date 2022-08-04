95cfd027ebf05de3f73625493782d89261f9bf72
Edgar Fabiano <eddy.led@gmail.com>
2022-08-04 10:35:43 -0300

    Exporting more functions

--- www/LivenessPlugin.js
+++ www/LivenessPlugin.js
@@ -1,7 +1,39 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'LivenessPlugin', 'coolMethod', [arg0]);
+exports.initialize = function () {
+    exec(
+        () => {},
+        () => console.log('init error'),
+        "LivenessPlugin",
+        "initialize",
+        []
+      );
+};
+
+exports.setThreshold = function (thresholdOption) {
+    if(thresholdOption === null or thresholdOption === undefined) {
+        thresholdOption = "balanced_very_high";
+    }
+    exec(
+        () => {},
+        () => {},
+        "LivenessPlugin",
+        "setThreshold",
+        [thresholdOption]
+      );
+};
+
+exports.setCamera = function (cameraOption) {
+    if(cameraOption === null or cameraOption === undefined) {
+        cameraOption = "front";
+    }
+    exec(
+        () => {},
+        () => {},
+        "LivenessPlugin",
+        "setCamera",
+        [cameraOption]
+      );
 };
 
 exports.capture = function (success, error) {
