70abc2af81657b254fc5018fb730cb298c216027
Edgar Fabiano <eddy.led@gmail.com>
2022-08-04 17:35:42 -0300

    fix

--- www/LivenessPlugin.js
+++ www/LivenessPlugin.js
@@ -8,3 +8,39 @@ exports.capture = function (success, error) {
     console.log('capturing');
     exec(success, error, 'LivenessPlugin', 'capture');
 };
+
+exports.initialize = function () {
+    exec(
+        () => {},
+        () => console.log('init error'),
+        "LivenessPlugin",
+        "initialize",
+        []
+      );
+};
+
+exports.setThreshold = function (thresholdOption) {
+    if(thresholdOption === null || thresholdOption === undefined) {
+        thresholdOption = "balanced_very_high";
+    }
+    exec(
+        () => {},
+        () => {},
+        "LivenessPlugin",
+        "setThreshold",
+        [thresholdOption]
+      );
+};
+
+exports.setCamera = function (cameraOption) {
+    if(cameraOption === null || cameraOption === undefined) {
+        cameraOption = "front";
+    }
+    exec(
+        () => {},
+        () => {},
+        "LivenessPlugin",
+        "setCamera",
+        [cameraOption]
+      );
+};
